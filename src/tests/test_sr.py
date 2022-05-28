3e3a771eae0f3fb1eeb15fbc794a15b7da207fb4
nicozu <nzulaica@fi.uba.ar>
2022-05-28 20:49:38 -0300

    test sr rdt

--- src/tests/test_sr.py
+++ src/tests/test_sr.py
@@ -73,8 +73,38 @@ def test_should_receive_data_big_buggy():
     assert output == data
 
 
-def test_should_send_and_receive_data():
+# FAILING - FIN missing
+@pytest.mark.slow
+def test_should_send_data():
     port = 57121 + 3
+    msg = b"Server: Hello"
+
+    listener = RDTListener("selective_repeat")
+    listener.bind(("127.0.0.1", port))
+    listener.listen(1)
+
+    def client(port):
+        client = SRSocket()
+        client.connect(("127.0.0.1", port))
+        cli_msg = client.recv(len(msg))
+        assert cli_msg == msg
+        client.close()
+
+    thread = Thread(target=client, args=[port])
+    thread.start()
+    socket = listener.accept()
+
+    socket.send(msg)
+
+    # Como todavia no esta el FIN y FINACK hay que joinear
+    # el otro thread antes de cerrar este socket
+    thread.join()
+    socket.close()
+    listener.close()
+
+
+def test_should_send_and_receive_data():
+    port = 57121 + 4
     msg_1 = b"Client: Hello"
     msg_2 = b"Server: Hello"
     msg_3 = b"Client: Bye"
