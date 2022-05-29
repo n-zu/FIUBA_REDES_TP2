558146ec5f4919a6592dfd9318e129b0e4895627
Tomas Civini <tcivini@gmail.com>
2022-05-28 22:44:26 -0300

    created upload and download prototype

--- src/args_client.py
+++ src/args_client.py
@@ -1,9 +1,14 @@
 import argparse
+from re import S
 
 
-def args_client():
+def args_client(upload):
+    if upload:
+        flag = "s"
+    else:
+        flag = "d"
     first = '%(prog)s  [ - h ] [ - v | -q ] [ - H ADDR ] '
-    second = '[ - p PORT ] [ - d FILEPATH ] [ - n FILENAME ]'
+    second = '[ - p PORT ] [ - ' + flag + ' FILEPATH ] [ - n FILENAME ]'
     parser = argparse.ArgumentParser(
         description='< command description >',
         usage=first + second)
@@ -20,8 +25,12 @@ def args_client():
         "-H", "--host", help="server IP address", type=str, metavar="")
     parser.add_argument(
         "-p", "--port", help="server port", type=str, metavar="")
-    parser.add_argument(
-        "-d", "--dst", help="destination file path", type=str, metavar="")
+    if upload:
+        parser.add_argument(
+            "-s", "--src", help="source file path", type=str, metavar="")
+    else:
+        parser.add_argument(
+            "-d", "--dst", help="destination file path", type=str, metavar="")
     parser.add_argument(
         "-n", "--name", help="file name", type=str, metavar="")
     return parser.parse_args()
